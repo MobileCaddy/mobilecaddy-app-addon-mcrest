8a257512887d7c79d74c023b75c4f862d2f6488b
Todd Halfpenny <todd@mobilecaddy.net>
2017-10-25 10:50:35 +0100

    Correcting cp of test file

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,5 +9,5 @@ testdestdir="../../www/js/services"
 if [ -d "$servicesdir" ]; then
 		echo "Directory exists!"
     cp js/mcrest.service.js $servicesdir
-    cp test/specs/mcresetservice.test.js $testdestdir
+    cp test/specs/mcrestservice.test.js $testdestdir
 fi
