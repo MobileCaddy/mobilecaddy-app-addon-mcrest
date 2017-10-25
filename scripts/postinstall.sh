2f142ed523868b85291b2719611b67870fe34eb4
Todd Halfpenny <todd@mobilecaddy.net>
2017-10-25 10:10:12 +0100

    Adding config call, adding more tests and correcting logger calls

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,9 +2,12 @@
 
 echo "Running postinstall.sh, from..."
 pwd
+
 servicesdir="../../www/js/services"
+testdestdir="../../www/js/services"
 
 if [ -d "$servicesdir" ]; then
 		echo "Directory exists!"
     cp js/mcrest.service.js $servicesdir
+    cp test/specs/mcresetservice.test.js $testdestdir
 fi
--- test/specs/mcrestservice.test.js
+++ test/specs/mcrestservice.test.js
@@ -35,6 +35,53 @@ describe('McRestService Unit Tests', function(){
     });
   });
 
+
+  describe('config', function(){
+
+    beforeEach(inject(function (_McRestService_) {
+        McRestService = _McRestService_;
+    }));
+
+    it ('should have default version', function(){
+      expect(McRestService._getApiVersion()).toBe("v40.0");
+    });
+
+    it ('should have default version, no apiVersion supplied', function(){
+      McRestService.config({});
+      expect(McRestService._getApiVersion()).toBe("v40.0");
+    });
+
+    it ('should reject invalid apiVersion - no "v"', function(){
+      expect(McRestService.config({apiVersion: "12.22"})).toBe(false);
+      expect(McRestService._getApiVersion()).toBe("v40.0");
+      expect(loggerMock.error).toHaveBeenCalled();
+    });
+
+    it ('should reject invalid apiVersion - alphas', function(){
+     expect( McRestService.config({apiVersion: "v1v.22"})).toBe(false);
+      expect(McRestService._getApiVersion()).toBe("v40.0");
+      expect(loggerMock.error).toHaveBeenCalled();
+    });
+
+    it ('should reject invalid apiVersion - too many decimals', function(){
+      expect(McRestService.config({apiVersion: "v12.345"})).toBe(false);
+      expect(McRestService._getApiVersion()).toBe("v40.0");
+      expect(loggerMock.error).toHaveBeenCalled();
+    });
+
+    it ('should update for correct apiVersion, 2 decimals', function(){
+      expect(McRestService.config({apiVersion: "v12.34"})).toBe(true);
+      expect(McRestService._getApiVersion()).toBe("v12.34");
+    });
+
+    it ('should update for correct apiVersion, 1 decimal', function(){
+      expect(McRestService.config({apiVersion: "v12.0"})).toBe(true);
+      expect(McRestService._getApiVersion()).toBe("v12.0");
+    });
+
+  });
+
+
   describe('setUpOauth', function() {
 
   beforeEach(inject(function (_McRestService_) {
